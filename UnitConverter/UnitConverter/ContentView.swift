aa795dd2509200f710dd8dc79e20707a12a354ec
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-04-25 21:02:12 +0200

    Day 19 - Temperature Unit Converter app - implement UI

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  @State private var inputValue = ""
+  @State private var inputUnit = ""
+  @State private var outputUnit = ""
+  
+  let temperatureUnits = ["Kelvin", "Celsius", "Fahrenheit"]
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      NavigationView {
+        Form {
+          Section(header: Text("Input value and choose unit")) {
+            TextField("Input", text: $inputValue).keyboardType(.decimalPad)
+            Picker("Input unit", selection: $inputUnit) {
+              ForEach(0 ..< temperatureUnits.count) {
+                Text("\(temperatureUnits[$0])")
+              }
+            }.pickerStyle(SegmentedPickerStyle())
+          }
+          
+          Section(header: Text("Choose output unit")) {
+            Picker("Output unit", selection: $outputUnit) {
+              ForEach(0 ..< temperatureUnits.count) {
+                Text("\(temperatureUnits[$0])")
+              }
+            }.pickerStyle(SegmentedPickerStyle())
+            Text("Result")
+          }
+        }.navigationTitle("Unit converter")
+      }
     }
 }
 
