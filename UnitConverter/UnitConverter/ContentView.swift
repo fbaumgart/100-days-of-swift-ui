5766670433afe3fab4b12c75182da294a1e6a970
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-04-25 21:35:32 +0200

    Day 19 - implement calculating units - project finished'

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -10,38 +10,59 @@ import SwiftUI
 struct ContentView: View {
   
   @State private var inputValue = ""
-  @State private var inputUnit = ""
-  @State private var outputUnit = ""
+  @State private var inputUnit = 0
+  @State private var outputUnit = 0
   
-  let temperatureUnits = ["Kelvin", "Celsius", "Fahrenheit"]
+  let temperatureUnits = TemparatureUnit.allCases
   
-    var body: some View {
-      NavigationView {
-        Form {
-          Section(header: Text("Input value and choose unit")) {
-            TextField("Input", text: $inputValue).keyboardType(.decimalPad)
-            Picker("Input unit", selection: $inputUnit) {
-              ForEach(0 ..< temperatureUnits.count) {
-                Text("\(temperatureUnits[$0])")
-              }
-            }.pickerStyle(SegmentedPickerStyle())
-          }
-          
-          Section(header: Text("Choose output unit")) {
-            Picker("Output unit", selection: $outputUnit) {
-              ForEach(0 ..< temperatureUnits.count) {
-                Text("\(temperatureUnits[$0])")
-              }
-            }.pickerStyle(SegmentedPickerStyle())
-            Text("Result")
-          }
-        }.navigationTitle("Unit converter")
-      }
+  var outputValue: Double {
+    guard let value = Double(inputValue) else { return 0 }
+    let inputTemperatureUnit = temperatureUnits[inputUnit].unit
+    let outputTemperatureUnit = temperatureUnits[outputUnit].unit
+    let measurement = Measurement(value: value, unit: inputTemperatureUnit)
+    let convertedResult = measurement.converted(to: outputTemperatureUnit)
+    return convertedResult.value
+  }
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section(header: Text("Input value and choose unit")) {
+          TextField("Input temperature", text: $inputValue).keyboardType(.decimalPad)
+          Picker("Input unit", selection: $inputUnit) {
+            ForEach(0 ..< temperatureUnits.count) {
+              Text("\(temperatureUnits[$0].rawValue)")
+            }
+          }.pickerStyle(SegmentedPickerStyle())
+        }
+        
+        Section(header: Text("Choose output unit")) {
+          Picker("Output unit", selection: $outputUnit) {
+            ForEach(0 ..< temperatureUnits.count) {
+              Text("\(temperatureUnits[$0].rawValue)")
+            }
+          }.pickerStyle(SegmentedPickerStyle())
+          Text("\(outputValue, specifier: "%.2f")").foregroundColor(.gray)
+        }
+      }.navigationTitle("Unit converter")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+enum TemparatureUnit: String, CaseIterable {
+  case Celsius, Kelvin, Fahrenheit
+  
+  var unit: UnitTemperature {
+    switch self {
+    case .Celsius: return .celsius
+    case .Fahrenheit: return .fahrenheit
+    case .Kelvin: return .kelvin
     }
+  }
 }
