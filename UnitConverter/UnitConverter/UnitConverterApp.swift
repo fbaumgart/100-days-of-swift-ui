7431aa73012fd3ba404cae198f8c68e5587b5346
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-04-25 20:31:25 +0200

    Day 19 - Unit Converterter init

---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Filip Baumgart on 25/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
