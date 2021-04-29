b571e2680ed23a19da1d6f6b510d871666333795
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-04-29 18:21:05 +0200

    Day 22 - finish the GuessTheFlag challenge

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+  @State private var correctAnswer = Int.random(in: 0...2)
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
+  @State private var playerScore = 0
+  @State private var alertMessage = Text("")
+  
   var body: some View {
     ZStack {
-      Color.red.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-      Text("Your content")
+      LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+        .edgesIgnoringSafeArea(.all)
+      VStack(spacing: 30) {
+        VStack {
+          Text("Tap the flag of").foregroundColor(.white)
+          Text(countries[correctAnswer]).foregroundColor(.white).font(.largeTitle)
+            .fontWeight(.black)
+        }
+        ForEach(0 ..< 3) { number in
+          Button(action: {
+            self.flagTapped(number)
+          }) {
+            Image(self.countries[number])
+              .renderingMode(.original).clipShape(Capsule())
+              .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+              .shadow(color: .black, radius: 2)
+          }
+        }
+        Spacer()
+      }
     }
+    .alert(isPresented: $showingScore) {
+      Alert(title: Text(scoreTitle), message: alertMessage, dismissButton: .default(Text("Continue")) {
+        self.askQuestion()
+      })
+    }
+  }
+  
+  func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+      scoreTitle = "Correct"
+      alertMessage = Text("Your score is \(playerScore)")
+      playerScore += 10
+    } else {
+      scoreTitle = "Wrong"
+      alertMessage = Text("That's the flag of \(countries[number])!")
+      playerScore -= 10
+    }
+    
+    showingScore = true
+  }
+  
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
   }
 }
 
