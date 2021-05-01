49cdacc232ed83e91d0a9258a55e5f0a46b0669c
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-05-01 20:13:33 +0200

    Day 24 - challenge - task no 1 - add Title ViewModifier

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,25 @@
 
 import SwiftUI
 
+struct Title: ViewModifier {
+  func body(content: Content) -> some View {
+          content
+              .font(.largeTitle)
+              .foregroundColor(.blue)
+      }
+}
+
+extension View {
+  func asTitle() -> some View {
+    self.modifier(Title())
+  }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .asTitle()
     }
 }
 
