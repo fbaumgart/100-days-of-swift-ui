0144bb033004405ea8d17e1b9f2fbecd3e5b2ea6
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-05-02 14:00:27 +0200

    Day 25 - create project for rock paper scissors challenge app

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Filip Baumgart on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
