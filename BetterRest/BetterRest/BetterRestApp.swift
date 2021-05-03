3473bac9d4bf6b40b7939b151a1bc1a5a7ee61fa
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-05-03 11:04:53 +0200

    Day 26 - create project for ML app - BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Filip Baumgart on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
