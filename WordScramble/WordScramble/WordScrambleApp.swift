4e9528f5bff1521707aba31e8101ac1ffd683fc0
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-05-06 16:53:14 +0200

    Day 29 - create WordScramble project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Filip Baumgart on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
