1d20aae27c81816ebf010806a3609d6bcdbfb49a
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-05-10 20:03:37 +0200

    Day 32 - create project for Animations

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Filip Baumgart on 10/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
