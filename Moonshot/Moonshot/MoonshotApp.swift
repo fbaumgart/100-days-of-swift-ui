c342f7ab4b44bf7ca59a54cc8cd109711393646b
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-06-03 10:42:33 +0200

    Day 39 - create project for Moonshot

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Filip Baumgart on 03/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
