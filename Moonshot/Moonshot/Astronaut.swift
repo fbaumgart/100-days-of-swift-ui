bb549f108892abedf69e5b13c616a67da43e63e8
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-06-04 10:27:52 +0200

    Day 40 - add assets + Decodable extension for Bundle

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,12 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Filip Baumgart on 04/06/2021.
+//
+
+struct Astronaut: Codable, Identifiable {
+  let id: String
+  let name: String
+  let description: String
+}
---
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,26 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Filip Baumgart on 04/06/2021.
+//
+
+extension Bundle {
+  func decode(_ file: String) -> [Astronaut] {
+    guard let url = self.url(forResource: file, withExtension: nil) else {
+      fatalError("Failed to locate \(file) in bundle.")
+    }
+    
+    guard let data = try? Data(contentsOf: url) else {
+      fatalError("Failed to load \(file) from bundle.")
+    }
+    
+    let decoder = JSONDecoder()
+    
+    guard let loaded = try? decoder.decode([Astronaut].self, from: data) else {
+      fatalError("Failed to decode \(file) from bundle.")
+    }
+    
+    return loaded
+  }
+}
