356575fc29e472772fb437f72483a6b7f4c2084f
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-06-04 11:04:37 +0200

    Day 40 - add layout + computed properties to mission struct

--- Moonshot/Moonshot/Mission.swift
+++ Moonshot/Moonshot/Mission.swift
@@ -5,14 +5,35 @@
 //  Created by Filip Baumgart on 04/06/2021.
 //
 
+import Foundation
+
 struct Mission: Codable, Identifiable {
+  
+  var displayName: String {
+    "Apollo \(id)"
+  }
+  
+  var image: String {
+    "apollo\(id)"
+  }
+  
+  var formattedLaunchDate: String {
+    if let launchDate = launchDate {
+      let formatter = DateFormatter()
+      formatter.dateStyle = .long
+      return formatter.string(from: launchDate)
+    } else {
+      return "N/A"
+    }
+  }
+  
   struct CrewRole: Codable {
     let name: String
     let role: String
   }
   
   let id: Int
-  let launchDate: String?
+  let launchDate: Date?
   let crew: [CrewRole]
   let description: String
 }
