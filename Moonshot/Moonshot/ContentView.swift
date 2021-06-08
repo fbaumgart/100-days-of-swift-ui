6bc108bf2284cdc331a98363a33051b763690a10
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-06-08 16:29:29 +0200

    Day 42 - complete 3rd task - add toogling between launch dates and crew members

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
   
   private let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
   private let missions: [Mission] = Bundle.main.decode("missions.json")
+  @State private var isShowingCrewNames = false
+  private let crewNames = "Crew names"
+  private let launchDates = "Launch dates"
   
   var body: some View {
     NavigationView {
@@ -24,12 +27,32 @@ struct ContentView: View {
           VStack(alignment: .leading) {
             Text(mission.displayName)
               .font(.headline)
-            Text(mission.formattedLaunchDate)
+            if isShowingCrewNames {
+              Text(getCrewNames(for: mission))
+            } else {
+              Text(mission.formattedLaunchDate)
+            }
+            
           }
         }
       }
       .navigationBarTitle("Moonshot")
+      .navigationBarItems(trailing: Button(isShowingCrewNames ? launchDates : crewNames, action: {
+        isShowingCrewNames.toggle()
+      }))
     }
+  }
+  
+  private func getCrewNames(for mission: Mission) -> String {
+    var crew = ""
+    mission.crew.forEach { crewMember in
+      astronauts.forEach { astronaut in
+        if astronaut.id == crewMember.name {
+          crew.append(astronaut.name + "\n")
+        }
+      }
+    }
+    return crew.trimmingCharacters(in: .newlines)
   }
 }
 
