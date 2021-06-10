fd85765f6e473515b8183c76056c112132cc2cc8
Filip Baumgart <filip.baumgart@rsqtechnologies.com>
2021-06-10 20:15:35 +0200

    Day 43 - add project for Drawing app

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Filip Baumgart on 10/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
